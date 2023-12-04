81157e58718c4819c7fc491b0340e3adf4a99847
unknown <ramithrodrigo@yahoo.com>
2023-12-04 11:06:48 +0530

    config

--- frontend/user_app/src/index.js
+++ frontend/user_app/src/index.js
@@ -4,18 +4,14 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppRoute from "./app-route/AppRoute";
 import { AuthProvider } from "@asgardeo/auth-react";
+import configData from "../public/config.json";
 
+window.configs = configData;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthProvider
-        config={ {
-            signInRedirectURL: "https://ffc88583-eeb1-4faf-87b5-41f431c73b41.e1-us-east-azure.choreoapps.dev",
-            signOutRedirectURL: "https://ffc88583-eeb1-4faf-87b5-41f431c73b41.e1-us-east-azure.choreoapps.dev",
-            clientID: "XNYgoWhNc3h24IcfpDh5OPbNIyoa",
-            baseUrl: "https://api.asgardeo.io/t/wso2shamin",
-            scope: [ "openid","profile", "email", "app_roles", "can_request_certificate_scope","create_certificate_request_scope","get_certificate_requests_scope", "get_grama_divisions_scope", "update_user_certificate_requests_scope"]
-        } }
+        config={ window.configs }
     >
       <AppRoute />
   </AuthProvider>
