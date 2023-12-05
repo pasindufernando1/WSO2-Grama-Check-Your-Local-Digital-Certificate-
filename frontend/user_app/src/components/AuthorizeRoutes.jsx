0ce4a294ff397a9b41c3a5de1196f12380a9839f
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-12-05 20:25:14 +0530

    add loading to prevent redirection

--- frontend/user_app/src/components/AuthorizeRoutes.jsx
+++ frontend/user_app/src/components/AuthorizeRoutes.jsx
@@ -1,42 +1,42 @@
-// this component is used for protect routes
 import { Outlet, useLocation, Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { useAuthContext } from "@asgardeo/auth-react";
 import { useEffect, useState } from "react";
 
 const AuthorizeRoutes = ({ allowedRoles }) => {
-  const {state, signIn, signOut, getAccessToken, getIDToken, getBasicUserInfo} = useAuthContext();
-  const location = useLocation(); // to redirect the user to where he/she came from
+    const { state, signIn, signOut, getAccessToken, getIDToken, getBasicUserInfo } = useAuthContext();
+    const location = useLocation();
+    const [userGroup, setUserGroup] = useState("");
+    const [isLoading, setIsLoading] = useState(true); // Add loading state
 
-  const [userGroup, setUserGroup] = useState("");
+    useEffect(() => {
+        const getAuthInfo = async () => {
+            if (!state.isAuthenticated) return;
 
-  useEffect(() => { 
-    const getAuthInfo = async () => {
-        if(!state.isAuthenticated) return;  // If not authenticated, return
-    
-        getBasicUserInfo().then((info) => {
-            console.log(info.groups[0]);
-            setUserGroup(info.groups[0]);
-        });
+            const info = await getBasicUserInfo();
+            setIsLoading(false); // Set loading state to false after fetching user info
+        };
+
+        getAuthInfo();
+    }, [getBasicUserInfo, state.isAuthenticated]);
+
+    if (isLoading) {
+        return <div>Loading...</div>; // Render a loading indicator while fetching user info
     }
 
-    getAuthInfo();
-  }
-    , []);
+    if (!state.isAuthenticated) {
+        return <Navigate to="/" state={{ from: location }} replace />;
+    }
 
-  if(!state.isAuthenticated) {
-    return <Navigate to="/" state={{ from: location }} replace />
-  }
-
-  if (allowedRoles.includes(userGroup)) { // If the user is authorized to access the route
-    return <Outlet /> 
-  } else {
-    return <Navigate to="/unauthorized" state={{ from: location }} replace />
-  }
+    if (allowedRoles.includes(userGroup)) {
+        return <Outlet />;
+    } else {
+        return <Navigate to="/unauthorized" state={{ from: location }} replace />;
+    }
 };
 
 AuthorizeRoutes.propTypes = {
-  allowedRoles: PropTypes.array,
+    allowedRoles: PropTypes.array,
 };
 
 export default AuthorizeRoutes;
