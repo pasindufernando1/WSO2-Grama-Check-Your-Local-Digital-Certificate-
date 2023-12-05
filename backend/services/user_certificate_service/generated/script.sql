07e0a6754d24daefe574f2e5654a61cb04d8f496
sineththamuditha <sineththamuditha@gmail.com>
2023-12-05 09:08:30 +0530

    Change certificate creation response to include the email of the gramaniladhari officer

--- backend/services/user_certificate_service/generated/script.sql
+++ backend/services/user_certificate_service/generated/script.sql
@@ -9,6 +9,7 @@ DROP TABLE IF EXISTS `GramaDivision`;
 CREATE TABLE `GramaDivision` (
 	`id` VARCHAR(191) NOT NULL,
 	`name` VARCHAR(191) NOT NULL,
+	`email` VARCHAR(191) NOT NULL,
 	PRIMARY KEY(`id`)
 );
 
