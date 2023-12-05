f3c4a45689770549c6e8a23b9d522c7a2221a0d6
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-12-05 20:52:30 +0530

    get certificate requests of the user

--- frontend/user_app/src/components/requests.jsx
+++ frontend/user_app/src/components/requests.jsx
@@ -8,6 +8,9 @@ import Button from "@mui/material/Button";
 import { AiFillBank } from "react-icons/ai";
 import { AiFillFileExcel } from "react-icons/ai";
 import { AiFillFile } from "react-icons/ai";
+import { useAuthContext } from "@asgardeo/auth-react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const DataCard = ({ data, description, icon: IconComponent }) => (
   <Card
@@ -59,68 +62,108 @@ const DataCard = ({ data, description, icon: IconComponent }) => (
     </Grid>
   </Card>
 );
-function requests() {
-  return (
-    <>
-      <Card
-        sx={{
-          p: 2,
-          mt: 1,
-          mb: 2,
-          backgroundColor: "#f6f6f6",
-          width: "96%",
-          height: "30%",
-        }}
-      >
-        <Grid container>
-          <Grid xs={10}>
-            <Typography
-              variant="h7"
-              sx={{
-                fontWeight: 400,
-                fontFamily: "Poppins",
-                fontWeight: 500,
-                color: "black",
-              }}
-            >
-              Hello Shamin,
-            </Typography>
-          </Grid>
-          <Grid xs={12} mt={1}>
-            <Typography
-              sx={{
-                color: "black",
-                textAlign: "left",
-                width: 1,
-                fontSize: {
-                  xs: 11,
-                  sm: 13,
-                  md: 13,
-                },
-              }}
-            >
-              Use the GramaCert to quickly obtain your certificates without a
-              hassle
-            </Typography>
-          </Grid>
+function Requests() {
+  const [displayName, setDisplayName] = useState("");
+
+  const { state, getBasicUserInfo, httpRequest } = useAuthContext();
+
+  const [userInfo, setUserInfo] = useState({});
+
+  const [userCertificateRequests, setUserCertificateRequests] = useState([]);
+
+  useEffect(() => {
+    const authInfo = async () => {
+      if (!state.isAuthenticated) return;
+
+      const info = await getBasicUserInfo();
+
+      setUserInfo(info);
+      setDisplayName(info.displayName);
+
+      const certificateResponseConfig = {
+        headers: {
+          headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/scim+json"
+          },
+          method: "GET",
+          URL: window.configs.resourceServerURLs[0] + "certificates/",
+        },
+      }
+
+      const certificateResponse = await httpRequest(certificateResponseConfig);
+
+      if (certificateResponse.status === 200) {
+        console.log(certificateResponse);
+        setUserCertificateRequests(certificateResponse.data);
+      }
+    }
+
+    authInfo();
+  }, []);
+
+return (
+  <>
+    <Card
+      sx={{
+        p: 2,
+        mt: 1,
+        mb: 2,
+        backgroundColor: "#f6f6f6",
+        width: "96%",
+        height: "30%",
+      }}
+    >
+      <Grid container>
+        <Grid xs={10}>
+          <Typography
+            variant="h7"
+            sx={{
+              fontWeight: 400,
+              fontFamily: "Poppins",
+              fontWeight: 500,
+              color: "black",
+            }}
+          >
+            Hello {displayName},
+          </Typography>
         </Grid>
-      </Card>
-      <Grid
-        container
-        sx={{
-          width: { xs: "100%", sm: "70vw" },
-        }}
-        justifyContent="center"
-      >
-        <Grid xs={12} sm={6}>
-          <Card sx={{ p: 4, backgroundColor: "#f9f9f9" }}>
-            <Stack
-              direction="column"
-              justifyContent="space-around"
-              alignItems="center"
-              spacing={2}
-            >
-              <img src={Check} width="40%" style={{ marginTop: 2 }} />
+        <Grid xs={12} mt={1}>
+          <Typography
+            sx={{
+              color: "black",
+              textAlign: "left",
+              width: 1,
+              fontSize: {
+                xs: 11,
+                sm: 13,
+                md: 13,
+              },
+            }}
+          >
+            Use the GramaCert to quickly obtain your certificates without a
+            hassle
+          </Typography>
+        </Grid>
+      </Grid>
+    </Card>
+    <Grid
+      container
+      sx={{
+        width: { xs: "100%", sm: "70vw" },
+      }}
+      justifyContent="center"
+    >
+      <Grid xs={12} sm={6}>
+        <Card sx={{ p: 4, backgroundColor: "#f9f9f9" }}>
+          <Stack
+            direction="column"
+            justifyContent="space-around"
+            alignItems="center"
+            spacing={2}
+          >
+            <img src={Check} width="40%" style={{ marginTop: 2 }} />
+            <Link to="/applyCert">
               <Button
                 variant="outlined"
                 sx={{
@@ -146,6 +189,8 @@ function requests() {
               >
                 Apply for Certificate
               </Button>
+            </Link>
+            <Link to="/checkstatus">
               <Button
                 variant="outlined"
                 sx={{
@@ -171,36 +216,37 @@ function requests() {
               >
                 Check Status
               </Button>
-            </Stack>
-          </Card>
-        </Grid>
-        <Grid
-          container
-          xs={12}
-          sm={5}
-          sx={{
-            mx: "auto",
-          }}
-        >
-          <DataCard
-            data="Grama Division Assigned"
-            description="Horana Wewala"
-            icon={AiFillBank}
-          />
-          <DataCard
-            data="Current Certificate Details"
-            description="Expiry : 2020/10/23"
-            icon={AiFillFileExcel}
-          />
-          <DataCard
-            data="Number of Obtained certificates"
-            description="10"
-            icon={AiFillFile}
-          />
-        </Grid>
+            </Link>
+          </Stack>
+        </Card>
       </Grid>
-    </>
-  );
+      <Grid
+        container
+        xs={12}
+        sm={5}
+        sx={{
+          mx: "auto",
+        }}
+      >
+        <DataCard
+          data="Grama Division Assigned"
+          description="Horana Wewala"
+          icon={AiFillBank}
+        />
+        <DataCard
+          data="Current Certificate Details"
+          description="Expiry : 2020/10/23"
+          icon={AiFillFileExcel}
+        />
+        <DataCard
+          data="Number of Obtained certificates"
+          description="10"
+          icon={AiFillFile}
+        />
+      </Grid>
+    </Grid>
+  </>
+);
 }
 
-export default requests;
+export default Requests;
